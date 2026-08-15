5f1dfc830ace7eba5458e689b8bd61aa1601b18c
FudouRyouho <fudozaraki001@hotmail.com>
2026-08-15 01:37:56 -0300

    fix: change integrations[] structure in config.ts added "editor.defaultFormatter" for vscode

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -28,18 +28,17 @@ export const config = configSchema.parse({
     host: process.env.DASHBOARD_SERVER_HOST || '127.0.0.1',
     port: Number(process.env.DASHBOARD_SERVER_PORT || 3000),
   },
-  integrations: {
-    sonarr: [
-      {
-        kind: 'sonarr',
-        id: 'sonarr',
-        name: 'Sonarr',
-        url: process.env.SERVER_2_URL || 'http://192.168.10.197',
-        port: 8989,
-        apiKey: process.env.SONARR_APIKEY ?? 'No API Key provided',
-      },
-    ],
-  },
+  integrations: [
+    {
+      kind: 'sonarr',
+      id: 'sonarr',
+      name: 'Sonarr',
+      url: process.env.SERVER_2_URL || 'http://192.168.10.197',
+      port: 8989,
+      apiKey: process.env.SONARR_APIKEY,
+    },
+  ],
+
 });
 
 export type Config = z.infer<typeof configSchema>;
